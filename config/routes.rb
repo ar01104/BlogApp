db2cce4f69361a1be1cb49968e94c28c00fad317
Anis <ar01104@surrey.ac.uk>
2019-12-08 22:49:23 +0000

    Added views and StyleSheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'contact', to: 'home#contact'
+  get 'home', to: 'home#homepage'
+  root 'home#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
