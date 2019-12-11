e259b8ce84ce800267583b5500cc627db570f403
Anis <ar01104@surrey.ac.uk>
2019-12-11 21:30:04 +0000

    Added contact mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default to: 'info@BlogApp.com', from: 'info@BlogApp.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'contact', to: 'home#contact'
   get 'home', to: 'home#homepage'
+  post 'request_contact', to: 'home#request_contact'
   root 'home#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
